e8811a1a0ae6f045fd7926ae238425ca657c60a8
James McGuire <james.mcguire@talk21.com>
2011-01-16 14:15:54 +0000

    Required file

---
+++ FurnitureObject.java
@@ -0,0 +1,16 @@
+
+public class FurnitureObject {
+
+	public String Name;
+	public float X;
+	public float Y;
+	public float Z;
+
+	FurnitureObject(String name, float XDimension, float YDimension, float ZDimension) {
+		Name = name;
+		X = XDimension;
+		Y = YDimension;
+		Z = ZDimension;
+	}
+
+}
