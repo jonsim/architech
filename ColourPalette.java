6a386959685b6e2938cf9f0165f115eba484fd19
Michael Jewell <mj9911@bris.ac.uk>
2011-03-13 20:58:47 +0000

    forgot to upload colourpalette file

---
+++ ColourPalette.java
@@ -0,0 +1,50 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.colorchooser.*;
+
+/**
+ *
+ * @author God
+ */
+public class ColourPalette extends JPopupMenu implements ChangeListener, MouseListener {
+
+    protected JColorChooser tcc;
+    public Color fillColour = Color.black;
+
+    ColourPalette() {
+        tcc = new JColorChooser(Color.black);
+        tcc.getSelectionModel().addChangeListener(this);
+        tcc.setBorder(BorderFactory.createTitledBorder("Choose Text Color"));
+        this.add(tcc, BorderLayout.PAGE_END);
+        tcc.setPreviewPanel(new JPanel());
+        this.addMouseListener(this);
+    }
+
+    @Override
+    public void hide() {
+        this.setVisible(false);
+    }
+
+    public void stateChanged(ChangeEvent e) {
+        fillColour = tcc.getColor();
+    }
+
+    public void mousePressed(MouseEvent e) {
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+        hide();
+    }
+}
