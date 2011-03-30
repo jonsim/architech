9c75c9f03ab9270acb8ed06e52e78ceea83ea2b3
James McGuire <james.mcguire@talk21.com>
2011-03-30 14:41:42 +0100

    Furniture dragging fix for going in/out and back in again. The furniture used to be deleted and stay deleted.

--- TwoDDropListener.java
+++ TwoDDropListener.java
@@ -8,16 +8,35 @@ import java.awt.Point;
 class TwoDDropListener implements DropTargetListener {
    public static final int acceptableActions = DnDConstants.ACTION_COPY;
    private TwoDPanel twoDPanel;
-   private Furniture inProgress;
    private ObjectBrowser objectBrowser;
-
-   private boolean entered = false;
-   private boolean exited = true;
+   
+   private FurnitureSQLData rawData; // rawData is not set to null after each drag
+   private Furniture inProgress; // inProgress is however...
 
    TwoDDropListener(TwoDPanel twoDPanel, ObjectBrowser objectBrowser) {
       if (twoDPanel == null || objectBrowser == null) throw new IllegalArgumentException("null parameter");
       this.twoDPanel = twoDPanel;
       this.objectBrowser = objectBrowser;
+   }
+
+   private Point scalePoint(Point p, double zoomScale) {
+      Point val = new Point();
+      val.setLocation(Math.round(p.getLocation().x / zoomScale),
+         Math.round(p.getLocation().y / zoomScale));
+      return val;
+   }
+
+   private FurnitureSQLData getRawDataFromEvent(DropTargetDragEvent e, DataFlavor chosen) {
+      try {
+         Object data = e.getTransferable().getTransferData(chosen);
+         if (data instanceof FurnitureSQLData) {
+            return (FurnitureSQLData) data;
+         }
+      } catch (UnsupportedFlavorException ufe) {
+      } catch (IOException ioe) {
+      }
+
+      return null;
    }
 
    /** Called by isDragOk. Checks to see if the drag flavor is acceptable */
@@ -30,7 +49,9 @@ class TwoDDropListener implements DropTargetListener {
    private DataFlavor chooseDropFlavor(DropTargetDropEvent e) {
       if (e.isLocalTransfer() && e.isDataFlavorSupported(TransferData.furniture)) {
          return TransferData.furniture;
-      } else return null;
+      } else {
+         return null;
+      }
       // return DataFlavor.stringFlavor if supported, etc...
    }
 
@@ -38,7 +59,9 @@ class TwoDDropListener implements DropTargetListener {
    private DataFlavor chooseDropFlavor(DropTargetDragEvent e) {
       if (/*e.isLocalTransfer() &&*/ e.isDataFlavorSupported(TransferData.furniture)) {
          return TransferData.furniture;
-      } else return null;
+      } else {
+         return null;
+      }
       // return DataFlavor.stringFlavor if supported, etc...
    }
 
@@ -68,38 +91,34 @@ class TwoDDropListener implements DropTargetListener {
    /** Called while a drag operation is ongoing, when the mouse pointer enters the
     *  operable part of the drop site for the DropTarget registered with this listener. */
    public void dragEnter(DropTargetDragEvent e) {
-System.out.println("ENTER");
+//System.out.println("ENTER");
 
-      DataFlavor chosen;
+      DataFlavor chosen = chooseDropFlavor(e);
 
-      if (!isDragOk(e) || (chosen = chooseDropFlavor(e)) == null) {
+      if (!isDragOk(e) || chosen == null) {
          e.rejectDrag();
+
       } else {
-         try {
-            Object data = e.getTransferable().getTransferData(chosen);
-            if (data instanceof FurnitureSQLData) {
-               Point p = scalePoint(e.getLocation(), twoDPanel.getZoomScale());
-               inProgress = new Furniture( (FurnitureSQLData) data, p, objectBrowser);
-
-               if( inProgress.isDoorWindow() ) {
-                  twoDPanel.getCoords().addDoorWindow(inProgress);
-               } else {
-                  twoDPanel.getCoords().addFurniture(inProgress);
-               }
-
-               twoDPanel.setFurnitureAsHandlerAndStart(inProgress);
-
-               e.acceptDrag(e.getDropAction());
-               
-            } else {
-               e.rejectDrag();
-            }
-
-         } catch (UnsupportedFlavorException ufe) {
+         rawData = getRawDataFromEvent(e, chosen);
+         if (rawData == null) {
             e.rejectDrag();
-         } catch (IOException ioe) {
-            e.rejectDrag();
+            return;
          }
+
+         Point p = scalePoint(e.getLocation(), twoDPanel.getZoomScale());
+         Furniture f = new Furniture( rawData, p, objectBrowser);
+
+         inProgress = f;
+
+         if (inProgress.isDoorWindow()) {
+            twoDPanel.getCoords().addDoorWindow(inProgress);
+         } else {
+            twoDPanel.getCoords().addFurniture(inProgress);
+         }
+
+         twoDPanel.setFurnitureAsHandlerAndStart(inProgress);
+
+//         e.acceptDrag(e.getDropAction());
       }
    }
 
@@ -107,7 +126,6 @@ System.out.println("ENTER");
     *  the operable part of the drop site for the DropTarget registered with this listener. */
    public void dragExit(DropTargetEvent e) {
 System.out.println("EXIT");
-
 
       if( inProgress.isDoorWindow() )
          twoDPanel.getCoords().deleteDoorWindow(inProgress);
@@ -124,27 +142,41 @@ System.out.println("EXIT");
    /** Called when a drag operation is ongoing, while the mouse pointer is still over
     * the operable part of the drop site for the DropTarget registered with this listener. */
    public void dragOver(DropTargetDragEvent e) {
-//System.out.println("    over");
       Point p = scalePoint(e.getLocation(), twoDPanel.getZoomScale());
+//      System.out.println(p);
 
-      // dragEnter is not called for every entry, so re-add the furniture if it
-      // was deleted from coords by dragExit. This will do nothing if already added
-      if( inProgress.isDoorWindow() ) {
-         //twoDPanel.getCoords().addDoorWindow(inProgress);
-         twoDPanel.getCoords().moveDoorWindow(inProgress, p);
+      if (inProgress == null) {
+         // DRAGENTER COPY
+         inProgress = new Furniture( rawData, p, objectBrowser);
 
+         if (inProgress.isDoorWindow()) {
+            twoDPanel.getCoords().addDoorWindow(inProgress);
+         } else {
+            twoDPanel.getCoords().addFurniture(inProgress);
+         }
+
+         twoDPanel.setFurnitureAsHandlerAndStart(inProgress);
+
+//         e.acceptDrag(e.getDropAction());
+         
       } else {
-         //twoDPanel.getCoords().addFurniture(inProgress);
-         twoDPanel.dropFurnitureMiddleHandlerCall(p);
-      }
+         // dragEnter is not called for every entry, so re-add the furniture if it
+         // was deleted from coords by dragExit. This will do nothing if already added
+         if( inProgress.isDoorWindow() ) {
+            twoDPanel.getCoords().moveDoorWindow(inProgress, p);
+         } else {
+            twoDPanel.dropFurnitureMiddleHandlerCall(p);
+         }
 
-      e.acceptDrag(e.getDropAction());
+//         e.acceptDrag(e.getDropAction());
+      }
    }
 
    /** Called when the drag operation has terminated with a drop on the operable
     *  part of the drop site for the DropTarget registered with this listener. */
    public void drop(DropTargetDropEvent e) {
-System.out.println("   drop");
+//System.out.println("   drop");
+
       if (!isDropOk(e)) {
          e.rejectDrop();
 
@@ -178,23 +210,18 @@ System.out.println("   drop");
 
    /** Called if the user has modified the current drop gesture. */
    public void dropActionChanged(DropTargetDragEvent e) {
-System.out.println("    CHANGED");
+//System.out.println("    CHANGED");
+
       if (!isDragOk(e)) {
-         if( inProgress.isDoorWindow() )
+         if( inProgress.isDoorWindow() ) {
             twoDPanel.getCoords().deleteDoorWindow(inProgress);
-         else
+         } else {
             twoDPanel.getCoords().delete(inProgress);
+         }
 
          e.rejectDrag();
       } else {
          e.acceptDrag(e.getDropAction());
       }
-   }
-
-   private Point scalePoint(Point p, double zoomScale) {
-      Point val = new Point();
-      val.setLocation(Math.round(p.getLocation().x / zoomScale),
-         Math.round(p.getLocation().y / zoomScale));
-      return val;
    }
 }
